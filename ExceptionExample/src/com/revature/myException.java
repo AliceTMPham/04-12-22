8ad9549287e0c112e1bbb168cf5de374e04c4ded
AliceTMPham <100544509+AliceTMPham@users.noreply.github.com>
2022-04-12 23:06:05 -0400

    exception example

---
+++ ExceptionExample/src/com/revature/myException.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+public class myException {
+	static void checkPartySize(int partySize) {
+		if (partySize > 6) {
+			throw new ArithmeticException("No more than 6 pokemon can be in your party at one time");
+		
+		}
+		else {
+			System.out.print("Party size accepted");
+		}
+	}
+}
+	
